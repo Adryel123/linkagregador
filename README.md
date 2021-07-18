f818f5726213b7b9ce46ce63a9979b8eaba74235
Richard <richardcgmello@gmail.com>
2021-07-18 18:01:04 -0300

    mod: app folders structure

--- README.md
+++ README.md
@@ -1,23 +1,3 @@
 # Agregador de Links
 
-## iniciar a virtualenv
-Bizu supremo é usar uma venv :)
-É um ambiente virtual para não ter problemas com versão do python
-Pra instalar:
-
-`$apt install virtualenv`
-
-Pra criar
-
-`$virtualenv venv -p python3.8`
-
-Pra ativar:
-
-<img src="screenshots/ativar-venv.png"/>
-
-Daí, "enter path" > .venv/bin/python3
-
-Pra instalar os pacotes necessários:
-(certifique-se de que o terminal esteja com venv na frente, se não, reinicie o VSCode)
-
-`pip install -r requirements.txt`
+Um webapp onde o usuário pode criar uma conta, se cadastrar e criar uma página que concentre links para as suas redes sociais.
---
+++ app/main.py
@@ -0,0 +1,121 @@
+#-----------------------------------------------------------------------
+# Agregador de Links
+#-----------------------------------------------------------------------
+
+from flask import Flask, session, redirect, url_for, request, render_template
+app = Flask(__name__)
+app.secret_key = 'adryelehgay'
+
+#-----------------------------------------------------------------------
+
+accounts = {
+    'holtzwallund': {
+        'nome': 'Richard',
+        'senha': '95'
+    }
+}
+
+trees = {
+    'holtzwallund': {
+        'Instagram': 'https://www.instagram.com/holtzwallund',
+        'Facebook': 'https://www.facebook.com/holtzwallund',
+        'Twitter': 'https://www.twitter.com/holtzwallund',
+        'Telegram': 'https://www.telegram.org/holtzwallund'
+    }
+}
+
+#-----------------------------------------------------------------------
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#-----------------------------------------------------------------------
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+#-----------------------------------------------------------------------
+
+@app.route('/signup/save', methods=['POST', 'GET'])
+def save_account():
+    if request.method == 'POST':
+        name = request.form['name']
+        user = request.form['user']
+        password = request.form['password']
+    else:
+        name = request.form['name']
+        user = request.form['user']
+        password = request.args.get('password')
+
+    global accounts
+    user = user.lower()
+    accounts[user] = {}
+    accounts[user]['nome'] = name.capitalize()
+    accounts[user]['senha'] = password
+    session['username'] = user
+    return redirect(url_for('edit', user=user))
+
+#-----------------------------------------------------------------------
+
+@app.route('/login')
+def login():
+    if 'username' in session:
+        user = session['username']
+        return redirect(url_for('edit', user=user))
+    else:
+        return render_template('login.html')
+
+#-----------------------------------------------------------------------
+
+@app.route('/login/verification', methods=['POST', 'GET'])
+def verif_login():
+    if request.method == 'POST':
+        user = request.form['user']
+        password = request.form['password']
+    else:
+        user = request.args.get('user')
+        password = request.args.get('password')
+
+    if (user in accounts) and (accounts.get(user).get('senha') == password):
+        session['username'] = user
+        return redirect(url_for('edit', user=user))
+    else:
+        return redirect(url_for('login'))
+
+#-----------------------------------------------------------------------
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+#-----------------------------------------------------------------------
+
+@app.route('/edit/<user>')
+def edit(user):
+    if 'username' in session:
+        user = session['username']
+        nome = accounts[user]['nome']
+        return f'Você está logado, {nome}, e essa é a página de edição'
+    else:
+        return redirect(url_for('login'))
+
+#-----------------------------------------------------------------------
+
+@app.route('/<user>')
+def tree(user):
+    if user in trees:
+        return render_template('tree.html', user=accounts.get(user).get('nome'), links=trees[user])
+    else:
+        return f'Quando houver links cadastrados pra {user}, essa página exibirá.'
+
+#-----------------------------------------------------------------------
+
+if __name__ == '__main__':
+    from livereload import Server
+    server = Server(app.wsgi_app)
+    server.serve(host='localhost', port=5000)
+    
+#-----------------------------------------------------------------------
