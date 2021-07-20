1f1e7e1994e608e946d3c10f6d397b99954ae2a9
Richard <richardcgmello@gmail.com>
2021-07-20 10:49:56 -0300

    mod: at least one line

--- app/static/scripts/dashboard.js
+++ app/static/scripts/dashboard.js
@@ -1,69 +1,61 @@
 // Cria uma linha
 function criarLinha() {
   // Cria o elemento linha
-  const linha = document.createElement("div")
-  linha.className = 'linha'
+  const linha = document.createElement("div");
+  linha.className = "linha";
   linha.innerHTML = `
     <input type="text" class="name" placeholder="Nome" />
     <input type="text" class="url" placeholder="Link" />
     <button class="botao botao_sub">-</button>
-  `
+  `;
 
   // Adiciona evento de remoção de linha ao botão
-  linha
-    .querySelector('.botao_sub')
-    .addEventListener("click", delLinha)
+  linha.querySelector(".botao_sub").addEventListener("click", delLinha);
 
-  // adiciona a linha ao documento
-  document
-    .querySelector('.container_linhas')
-    .appendChild(linha)
+  // Adiciona a linha ao documento
+  document.querySelector(".container_linhas").appendChild(linha);
 }
 
 // Deleta uma linha
 function delLinha(event) {
-  event.target.parentElement.remove()
+  event.target.parentElement.remove();
+
+  // Ao menos uma linha
+  if (!document.querySelector(".linha")) {
+    criarLinha();
+  }
 }
 
 // Retorna uma lista com cada nome e url
 function capturarDados() {
   // Array com as linhas
-  const linhas = document.querySelector('.container_linhas').childNodes
+  const linhas = document.querySelector(".container_linhas").childNodes;
 
   // Para cada linha, captura nome e url
-  const links = []
-  linhas.forEach(linha => {
-    const nome = linha.querySelector('.name').value
-    const url = linha.querySelector('.url').value
-    links.push({ nome, url })
-  })
+  const links = [];
+  linhas.forEach((linha) => {
+    const nome = linha.querySelector(".name").value;
+    const url = linha.querySelector(".url").value;
+    links.push({ nome, url });
+  });
 
-  return links
+  return links;
 }
 
 // Faz uma requisição PUT para o endereço /edit
 // Passa no corpo os dados e o usuário
 function salvarDados() {
-  const dados = capturarDados()
-  const user = window.location.pathname.split('/')[2]
+  const dados = capturarDados();
+  const user = window.location.pathname.split("/")[2];
 
-  fetch('/edit', {
-    method: 'PUT',
+  fetch("/edit", {
+    method: "PUT",
     body: { ...dados, user },
-  }).then(console.log)
+  }).then(console.log);
 }
 
 // Adiciona função ao botão de adicionar linha
-document
-  .querySelector('.botao_add')
-  .addEventListener('click', criarLinha)
+document.querySelector(".botao_add").addEventListener("click", criarLinha);
 
 // Adiciona função ao botão de salvar
-document
-  .querySelector('.botao_salvar')
-  .addEventListener('click', salvarDados)
-
-// Ao menos uma linha
-if (!document.querySelector('.linha')) {
-  criarLinha()
-}
+document.querySelector(".botao_salvar").addEventListener("click", salvarDados);
